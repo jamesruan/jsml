898719a52293ac53d68648912c96c6ec3c09c991
James Ruan <ruanbeihong@gmail.com>
2014-03-30 13:01:39 +0800

    init

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ jsml
 ====
 
 Javascript object to DOM translator, inspired by https://github.com/caldwell/JSML
+
+This code is used with node.js.
+Use browserified jquery as library.
+
+'broweserify -r jquery -o bundle.js'
---
+++ jsml.js
@@ -0,0 +1,125 @@
+jQuery = require('jquery');
+(function($){
+    $.fn.jsml = function(value, wl){
+        var whitelist = [
+            "article",
+            "hr",
+            "br",
+            "h1",
+            "h2",
+            "h3",
+            "h4",
+            "h5",
+            "h6",
+            "small",
+            "p",
+            "blockquote",
+            "blockquote",
+            "code",
+            "pre",
+            "div",
+            "abbr",
+            "strong",
+            "em",
+            "mark",
+            "ins",
+            "del",
+            "i",
+            "cite",
+            "code", 
+            "span", 
+            "pre",
+            "ul",
+            "ol",
+            "li",
+            "a",
+            "table",
+            "caption",
+            "tbody",
+            "tr",
+            "th",
+            "td",
+            "img",
+            "figure"
+
+            ];
+
+        whitelist = $.extend([], whitelist, wl);
+
+        /* Translation rule:
+         * [tagname {attr} inside] =>
+         * OBJ(<tagname attr> inside </tagname>)
+         * [tagname [tagname2 inside2] inside] =>
+         * [tagname OBJ(<tagname2> inside2 </tagname2>) inside]
+         */
+
+        this.empty();
+        value = value.reduce(toHTML)
+            this.append(value[0]);
+
+        function isTag(value){
+            return (whitelist.indexOf(value)>=0);
+        }
+
+        function toHTML(prev, cur, index, array){
+            if(prev.constructor === Array){
+                if(prev.length === 1){
+                    prev = prev[0];
+                }
+                else{
+                    prev = prev.reduce(toHTML)[0];
+                }
+            }
+            if(prev.constructor === String){
+                if(isTag(prev)){
+                    var el=document.createElement(prev);
+                    prev = $(el);
+                }
+                else{
+                    $.error("Invalid tag name:" + prev);
+                    return;
+                }
+            }
+            //prev is head of the array and should now be an JQuery wrapped DOM, so test the cur
+            if(cur.constructor === Object){	//attr
+                for(var attr in cur){
+                    if(attr === "class")
+                    {
+                        prev.addClass(cur[attr]);
+                    }else{
+                        prev.attr(attr, cur[attr]);
+                    }
+                }
+                return prev;
+            }
+            else if(cur.constructor === Array){	//child
+                if(cur.length === 1){
+                    cur = cur[0];
+                }
+                else{
+                    cur = cur.reduce(toHTML)[0];
+                }
+                if(cur.constructor === String){
+                    if(isTag(cur)){
+                        var el=document.createElement(cur);
+                        cur = $(el);
+                    }
+                    else{
+                        $.error("Invalid tag name:" + cur);
+                        return;
+                    }
+                }
+                return prev.append(cur);
+            }
+            else if(cur.constructor === String){
+                prev.append(cur);
+                return prev;
+            }
+            else{
+                $.error("Invalid format:" + cur.toString());
+                return;
+            }
+        }
+        return this;
+    };
+})(jQuery);
